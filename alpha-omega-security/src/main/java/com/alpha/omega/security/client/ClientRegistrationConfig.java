f915db8350504ae6558396fa8fa0293230db7283
glawson6-ao <gregory.lawson@alphaomega.com>
2024-02-26 16:36:29 -0500

    Updating service for repositories

--- alpha-omega-security/src/main/java/com/alpha/omega/security/client/ClientRegistrationConfig.java
+++ alpha-omega-security/src/main/java/com/alpha/omega/security/client/ClientRegistrationConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories(basePackages = {"com.alpha.omega.security"})
+@EnableRedisRepositories(basePackages = {"com.alpha.omega.security.client"})
 public class ClientRegistrationConfig {
 
     //
--- alpha-omega-security/src/main/java/com/alpha/omega/security/idprovider/keycloak/KeyCloakConfig.java
+++ alpha-omega-security/src/main/java/com/alpha/omega/security/idprovider/keycloak/KeyCloakConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.function.Function;
 
 
-@Configuration
+//@Configuration
 @EnableConfigurationProperties(value = {KeyCloakIdpProperties.class})
 public class KeyCloakConfig {
 
